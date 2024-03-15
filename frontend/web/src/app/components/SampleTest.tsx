241f37a4bb90d8af918f37d0d50209a9bffbf731
Ashfaq Nisar <ashfaqnisar@celeritaz.com>
2024-03-15 10:19:07 +0530

    feat: Added the tailwind and updated the page.

--- frontend/web/src/app/components/SampleTest.tsx
+++ frontend/web/src/app/components/SampleTest.tsx
@@ -2,20 +2,23 @@
 
 import { useState } from "react";
 
-import styles from "~/app/page.module.css";
 import { trpc } from "~/app/trpc";
 
 const SampleTest = () => {
+  const [count, setCount] = useState(0);
   const [response, setResponse] = useState("");
 
   const makeApiCall = async () => {
-    const res = await trpc.hello.query({ name: "world" });
+    const res = await trpc.hello.query({ name: "Test" + count });
+    setCount(count + 1);
     setResponse(res);
   };
   return (
     <div>
-      <h1>{response}</h1>
-      <button className={styles.button} onClick={makeApiCall}>
+      <h1>
+        Client Render: Response from the api <q>{response ? response : "Empty"}</q>
+      </h1>
+      <button className={"px-4 py-2 bg-blue-600 rounded text-white"} onClick={makeApiCall}>
         Make api call
       </button>
     </div>
