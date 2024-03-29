dc45b3492297c0ea0e2f10904bddfa58af40a3b4
Ashfaq Nisar <ashfaqnisar@celeritaz.com>
2024-03-30 03:33:42 +0530

    fix: Updated the trpc structure for the application.

--- apps/frontend/src/trpc.ts
+++ apps/frontend/src/trpc.ts
@@ -1,10 +1,14 @@
 import { type AppRouter } from "@backend/src/modules/trpc/trpc.router";
-import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import { createTRPCProxyClient, httpBatchLink, loggerLink } from "@trpc/client";
 
 export const trpc = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: "http://localhost:8080/trpc" // you should update this to use env variables
+      url: process.env.NEXT_PUBLIC_API_ENDPOINT + "/trpc"
+    }),
+    loggerLink({
+      enabled: (op) =>
+        process.env.NODE_ENV === "development" || (op.direction === "down" && op.result instanceof Error)
     })
   ]
 });
