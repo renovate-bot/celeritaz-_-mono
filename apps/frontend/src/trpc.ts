391eae74195985f3ff99001ce8d7923b73754a58
Ashfaq Nisar <ashfaqnisar@celeritaz.com>
2024-04-01 21:47:12 +0530

    CLR-80: Added the env variables to the next app.

--- apps/frontend/src/trpc.ts
+++ apps/frontend/src/trpc.ts
@@ -1,14 +1,16 @@
 import { type AppRouter } from "@backend/src/modules/trpc/trpc.router.ts";
 import { createTRPCProxyClient, httpBatchLink, loggerLink } from "@trpc/client";
 
+import { env } from "~/env.ts";
+
 export const trpc = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: process.env.NEXT_PUBLIC_API_ENDPOINT + "/trpc"
+      url: env.NEXT_PUBLIC_API_ENDPOINT + "/trpc"
     }),
     loggerLink({
       enabled: (op) =>
-        process.env.NODE_ENV === "development" || (op.direction === "down" && op.result instanceof Error)
+        env.NODE_ENV === "development" || (op.direction === "down" && op.result instanceof Error)
     })
   ]
 });
