9b7f702617692fabe0950e40d931ac749dd8610d
Ashfaq Nisar <ashfaqnisar@celeritaz.com>
2024-04-17 12:13:12 +0530

    feat: Added the eslint & prettier and required deps.

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,39 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions & import('@ianvs/prettier-plugin-sort-imports').PluginConfig} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+  singleQuote: false,
+  printWidth: 100,
+  bracketSameLine: true,
+  endOfLine: "auto",
+  semi: true,
+  trailingComma: "none",
+  // This plugin's options
+  importOrder: [
+    "<BUILTIN_MODULES>",
+    "",
+    "^(react/(.*)$)|^(react$)",
+    "^(react-dom/(.*)$)|^(react-dom$)",
+    "^(next/(.*)$)|^(next$)",
+    "",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^(~/trpc/(.*)$)|^(~/trpc$)",
+    "",
+    "^(~/providers/(.*)$)|^(~/providers$)",
+    "^(~/utils/(.*)$)|^(~/utils)",
+    "^(~/redux/(.*)$)|^(~/redux)",
+    "^(~/shared/custom(.*)$)|^(~/shared/custom$)",
+    "^(~/shared/shadcn(.*)$)|^(~/shared/shadcn$)",
+    "",
+    "^~/(.*)$",
+    "",
+    "^\\../",
+    "",
+    "^\\./",
+    "",
+    "<TYPES>"
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"]
 };
 
 export default config;
